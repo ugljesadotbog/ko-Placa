dc2bf8c730e585606d624a0beeff08878119252d
ugljesaDotbog <rakonjac.ugljesa@outlook.com>
2025-04-03 17:41:43 +0200

    added sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+Sentry.init({
+  dsn: "https://99d406d08f445ffd457e5e1fb46e8c1d@o4509090073739269.ingest.de.sentry.io/4509090075705424",
+});
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), sentryVitePlugin({
+    org: "ugljesa",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
